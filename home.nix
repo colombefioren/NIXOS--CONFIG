d7fdcbac79837ceceb1fd717501266325286e0a3
colombefioren <colomberakotonjanahary@gmail.com>
2026-09-16 13:28:13 +0300

    feat: new conf

--- home.nix
+++ home.nix
@@ -526,6 +526,8 @@
     syntaxHighlighting.enable = true;
 
     initContent = ''
+      export PATH="$HOME/.local/bin:$PATH"
+
       export NVM_DIR="$HOME/.nvm"
       [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
       [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
